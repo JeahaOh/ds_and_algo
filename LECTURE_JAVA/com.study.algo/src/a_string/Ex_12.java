c5f222aead45a997ef010b271c1db31939ae6c77
JeahaOh <jeaha1217@gmail.com>
2022-08-29 22:16:56 +0900

    20220829

---
+++ LECTURE_JAVA/com.study.algo/src/a_string/Ex_12.java
@@ -0,0 +1,25 @@
+package a_string;
+
+import java.util.Scanner;
+
+public class Ex_12 {
+  public static String solution(int n, String s) {
+    String answer = "";
+    
+    for (int i = 0; i < n; i++) {
+      String t = s.substring(0, 7).replace('#', '1').replace('*', '0');
+      int ascii = Integer.parseInt(t, 2);
+      answer += (char) ascii;
+      s = s.substring(7);
+    }
+    
+    return answer;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int length = sc.nextInt();
+    String str = sc.next();
+    System.out.println(solution(length, str));
+  }
+}
---
+++ LECTURE_JAVA/com.study.algo/src/b_array/Ex_01.java
@@ -0,0 +1,27 @@
+package b_array;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Ex_01 {
+  public static ArrayList<Integer> solution(int n, int[] nums) {
+    ArrayList<Integer> answer = new ArrayList<>();
+    answer.add(nums[0]);
+    
+    for (int i = 1; i < n; i++) {
+      if (nums[i - 1] < nums[i]) answer.add(nums[i]);
+    }
+    
+    return answer;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+    for (int i = 0; i < n; i++) {
+      nums[i] = sc.nextInt();
+    }
+    for (int x : solution(n, nums)) {
+      System.out.print(x + " ");
+    }
+  }
+}
