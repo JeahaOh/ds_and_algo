1dc5b5f7e1be594ccc43107fd43ff4e3cacbae8b
JeahaOh <jeaha1217@gmail.com>
2022-08-30 21:29:31 +0900

    20220830

---
+++ LECTURE_JAVA/com.study.algo/src/b_array/Ex_03.java
@@ -0,0 +1,33 @@
+package b_array;
+
+import java.util.Scanner;
+
+public class Ex_03 {
+  public static String[] solution(int n, int[][] arr) {
+    String[] answer = new String[n];
+    for (int i = 0; i < n; i++) {
+      if (arr[i][0] == arr[i][1]) answer[i] = "D";
+      else if (arr[i][0] == 1 && arr[i][1] == 3) answer[i] = "A";
+      else if (arr[i][0] == 2 && arr[i][1] == 1) answer[i] = "A";
+      else if (arr[i][0] == 3 && arr[i][1] == 2) answer[i] = "A";
+      else answer[i] = "B";
+    }
+    return answer;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[][] arr = new int[n][2];
+    for (int i = 0; i < n; i++) {
+      arr[i][0] = sc.nextInt();
+    }
+    for (int i = 0; i < n; i++) {
+      arr[i][1] = sc.nextInt();
+    }
+    
+    for (String s : solution(n, arr)) {
+      System.out.println(s);
+    }
+  }
+}
---
+++ LECTURE_JAVA/com.study.algo/src/b_array/Ex_05.java
@@ -0,0 +1,23 @@
+package b_array;
+
+import java.util.Scanner;
+
+public class Ex_05 {
+  public static int solution(int n) {
+    int answer = 0;
+    int[] nums = new int[n + 1];
+    for (int i = 2; i <= n; i++) {
+      if (nums[i] == 0) {
+        answer++;
+        for (int j = i; j <= n; j = j + i) nums[j] = 1;
+      }
+    }
+    return answer;
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(solution(n));
+  }
+}
