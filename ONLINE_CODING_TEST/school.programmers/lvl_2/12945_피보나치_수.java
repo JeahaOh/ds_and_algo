1fd64c3f9576313674b2f0ad11dc2d50bf2476e8
JeahaOh <jeaha1217@gmail.com>
2022-10-17 14:26:53 +0900

    20221017

---
+++ ONLINE_CODING_TEST/school.programmers/lvl_2/12945_피보나치_수.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int solution(int n) {
+    int answer = 0;
+    int fibo1 = 0, fibo2 = 1;
+    
+    for (int i = 2; i <= n; i++) {
+      answer = (fibo1 + fibo2) % 1234567;
+      fibo1 = fibo2;
+      fibo2 = answer;
+    }
+    
+    return answer;
+  }
+}
