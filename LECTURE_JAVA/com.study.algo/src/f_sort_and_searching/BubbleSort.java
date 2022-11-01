87ed57949ff175de8d69b1720996ebb959012eee
JeahaOh <jeaha1217@gmail.com>
2022-11-01 11:59:34 +0900

    20221101

---
+++ LECTURE_JAVA/com.study.algo/src/f_sort_and_searching/BubbleSort.java
@@ -0,0 +1,53 @@
+package f_sort_and_searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+//    public static int[] solution(int n, int[] arr) {
+//        int temp;
+//        for (int i = n - 1; i > 0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//
+//            System.out.println(Arrays.toString(arr));
+//        }
+//
+//        return arr;
+//    }
+    
+    
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+        
+        return arr;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
