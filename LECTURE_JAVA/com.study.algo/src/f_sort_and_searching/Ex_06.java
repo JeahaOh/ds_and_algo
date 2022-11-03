e6e5cc80b868ca53aaf3b4bdd786f8265165b6a7
JeahaOh <jeaha1217@gmail.com>
2022-11-03 11:59:51 +0900

    20221103

---
+++ LECTURE_JAVA/com.study.algo/src/f_sort_and_searching/Ex_06.java
@@ -0,0 +1,30 @@
+package f_sort_and_searching;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ex_06 {
+    public static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int [] temp = arr.clone();
+        Arrays.sort(temp);
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != temp[i]) answer.add(i + 1);
+        }
+        
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        
+        for (int x : solution(n, arr)) System.out.print(x + " ");
+    }
+}
