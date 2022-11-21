1479eeb0792fd82aff8da3732dc79d1322f40a3f
JeahaOh <jeaha1217@gmail.com>
2022-11-21 17:06:15 +0900

    2022 11 21

---
+++ LECTURE_JAVA/com.study.algo/src/g_recursive_tree_graph_dfs_bfs/Ex_02.java
@@ -0,0 +1,16 @@
+package g_recursive_tree_graph_dfs_bfs;
+
+// 재귀함수를 이용해서 2진수 출력하기
+public class Ex_02 {
+    public static void dfs(int n) {
+        if (n == 0) return;
+        
+        dfs(n / 2);
+        System.out.print(n % 2 + " ");
+    
+    }
+    
+    public static void main(String[] args) {
+        dfs(11);
+    }
+}
