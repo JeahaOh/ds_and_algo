f5ef4485a6347f4f2c5a4b0ced895a8e1efaadf1
JeahaOh <jeaha1217@gmail.com>
2023-01-01 20:36:40 +0900

    20230101

---
+++ LECTURE_JAVA/com.study.algo/src/h_advance_dfs_and_bfs/Ex_01.java
@@ -0,0 +1,45 @@
+package h_advance_dfs_and_bfs;
+
+import java.util.Scanner;
+
+/**
+ * 합이 같은 부분집합
+ * Ex_01 이미지 참고
+ */
+public class Ex_01 {
+    static String answer = "NO";
+    static int depth, total = 0;
+    static boolean flag = false;
+    
+    public static void dfs(int turn, int sum, int[] arr) {
+        if (flag) return;
+        if (sum > total / 2) return;
+        
+        if(turn == depth) {
+          if ((total - sum) == sum) {
+              answer = "YES";
+              flag = true;
+          }
+        } else {
+          dfs(turn + 1, sum + arr[turn], arr);
+          dfs(turn + 1, sum, arr);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        depth = sc.nextInt();
+        int[] arr = new int[depth];
+        
+        for (int i = 0; i < depth; i++) {
+          arr[i] = sc.nextInt();
+          total += arr[i];
+        }
+        
+        dfs(0, 0, arr);
+        
+        System.out.println(answer);
+    }
+}
+// 6
+// 1 3 5 6 7 10
---
+++ LECTURE_JAVA/com.study.algo/src/h_advance_dfs_and_bfs/Point.java
@@ -0,0 +1,10 @@
+package h_advance_dfs_and_bfs;
+
+class Point {
+    public int x, y;
+    
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
