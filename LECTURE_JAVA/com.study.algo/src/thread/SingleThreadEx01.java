4ab34083985870365bd088f2faa978d6ef8939fb
JeahaOh <jeaha1217@gmail.com>
2023-01-23 22:37:56 +0900

    2023 01 23

---
+++ LECTURE_JAVA/com.study.algo/src/thread/SingleThreadEx01.java
@@ -0,0 +1,31 @@
+package thread;
+
+public class SingleThreadEx01 extends Thread {
+    private int[] temp;
+    
+    public SingleThreadEx01(String threadname) {
+        super(threadname);
+        temp = new int[10];
+        
+        for (int start = 0; start < temp.length; start++) {
+            temp[start] = start;
+        }
+    }
+    
+    public void run() {
+        for (int start : temp) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            System.out.println("thread name : " + currentThread().getName());
+            System.out.println("temp value : " + start);
+        }
+    }
+    
+    public static void main(String[] args) {
+        SingleThreadEx01 st = new SingleThreadEx01("first");
+        st.start();
+    }
+}
---
+++ LECTURE_JAVA/com.study.algo/src/thread/SingleThreadEx02.java
@@ -0,0 +1,35 @@
+package thread;
+
+import static java.lang.Thread.currentThread;
+
+public class SingleThreadEx02 implements Runnable {
+    
+    private int[] temp;
+    
+    public SingleThreadEx02() {
+        temp = new int[10];
+        for (int start = 0; start < temp.length; start++) {
+            temp[start] = start;
+        }
+    }
+    
+    @Override
+    public void run() {
+        for (int start : temp) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+            
+            System.out.println("thread name : " + currentThread().getName());
+            System.out.println("temp value : " + start);
+        }
+    }
+    
+    public static void main(String[] args) {
+        SingleThreadEx02 ct = new SingleThreadEx02();
+        Thread t = new Thread(ct, "first");
+        t.start();
+    }
+}
