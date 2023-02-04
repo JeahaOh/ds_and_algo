ca9ef87671e3b8879ee0dfe810e32e4958e9d5b7
JeahaOh <jeaha1217@gmail.com>
2023-02-04 22:40:40 +0900

    2023 02 04

---
+++ LECTURE_JAVA/com.study.algo/src/thread/ejava/ex07/Cal.java
@@ -0,0 +1,23 @@
+package thread.ejava.ex07;
+
+public class Cal {
+    private int memo;
+    
+    public int getMemo() {
+        return this.memo;
+    }
+    
+    public void setMemo(int memo) {
+        synchronized(this) {
+            this.memo = memo;
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        
+            System.out.println(Thread.currentThread().getName() + " : " + this.memo);
+            
+        }
+    }
+}
---
+++ LECTURE_JAVA/com.study.algo/src/thread/ejava/ex07/User1.java
@@ -0,0 +1,15 @@
+package thread.ejava.ex07;
+
+public class User1 extends Thread {
+    private Cal cal;
+    
+    public void setCal(Cal cal) {
+        this.setName(this.getClass().getSimpleName());
+        this.cal = cal;
+    }
+    
+    @Override
+    public void run() {
+        this.cal.setMemo(Integer.MAX_VALUE);
+    }
+}
