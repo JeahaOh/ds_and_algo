e314b4a15b32873a0d354a2e00f9bfb9d7c363bb
JeahaOh <jeaha1217@gmail.com>
2023-03-21 14:03:58 +0900

    2023 03 21

--- LECTURE_JAVA/com.study.algo/src/do_it/chap04/_01/IntStackTester.java
+++ LECTURE_JAVA/com.study.algo/src/do_it/chap04/_01/IntStackTester.java
@@ -3,6 +3,11 @@ package do_it.chap04._01;
 import do_it.Input;
 
 public class IntStackTester {
+
+    private static final String menuInfo = "[1] push   [2] pop    [3] peek" +
+                                   "\n[4] dump   [5] search [6] clear" +
+                                   "\n[7] status [9] exit\n";
+
     public static void main(String[] args) {
         IntStack s = new IntStack(64);
         
@@ -10,11 +15,12 @@ public class IntStackTester {
             System.out.println("\n" + ("-".repeat(10)));
             System.out.printf("NOW DATA : %d / %d\n", s.size(), s.getCapacity());
             
-            int menu = Input.integer("(1) push (2) pop (3) peek (4) dump (0) exit");
-            if (menu < 1 || menu > 4) break;
+            int selectedMenu = Input.integer(menuInfo);
+            //System.out.println(menu);
+            if (selectedMenu == 9) break;
             
             int x;
-            switch (menu) {
+            switch (selectedMenu) {
                 case 1 :
                     // push
                     x = Input.integer("DATA");
@@ -46,8 +52,33 @@ public class IntStackTester {
                     break;
                 
                 case 4 :
+                    // dump
                     s.dump();
                     break;
+
+                case 5 :
+                    // search
+                    int target = Input.integer("TARGET TO SEARCH");
+                    int n = s.indexOf(target);
+
+                    if (n >= 0)
+                        System.out.println((s.size() - n) + " FROM THE TOP");
+                    else
+                        System.out.println("NO DATA");
+                    break;
+                
+                case 6 :
+                    // clear
+                    s.clear();
+                    break;
+                
+                case 7 :
+                    // print
+                    String status = String.format("CAPACITY : %d\nSIZE : %d\nIS EMPTY : %b\nIS FULL : %b", s.getCapacity(), s.size(), s.isEmpty(), s.isFull());
+                    System.out.println(status);
+                    break;
+                    
+                default : System.exit(1);
             }
         }
     }
