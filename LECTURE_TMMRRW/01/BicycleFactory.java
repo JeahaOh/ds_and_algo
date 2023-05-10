2ae9c721eaa057d77dff853813bc5d77ca9fec5d
JeahaOh <jeaha1217@gmail.com>
2023-05-10 21:49:42 +0900

    2023 05 10

---
+++ LECTURE_TMMRRW/01/BicycleFactory.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int solution(int[][] cost, int[][] order) {
+        int answer = 0;
+        
+        int maxMonth = 0;
+        for (int[] o : order) maxMonth = Math.max(maxMonth, o[0]);
+        
+        int[] monthlyOrder = new int[maxMonth];
+        int need = 0;
+        int made = 0;
+        for (int[] o : order) {
+            // order is not sorted!
+            monthlyOrder[o[0] - 1] += o[1];
+            need += o[1];
+        }
+        
+        for (int i = 0; i < cost.length - 1; i++) {
+            if (need == 0 || made >= need) break;
+            
+            int price = cost[i][1];
+            int limit = cost[i + 1][0] - cost[i][0];
+            int rest = 0;
+            
+            for (int j = 0; j < maxMonth; j++) {
+                if (need == 0 || made >= need) break;
+                
+                int making = Math.min(limit, need - made);
+                
+                answer += making * price;
+                made += making;
+                need -= monthlyOrder[j];
+                
+                // not need to supply
+                if (monthlyOrder[j] == 0) continue;
+                
+                int delivery = Math.min(made, monthlyOrder[j]);
+                
+                // supply from pre-made
+                made -= delivery;
+                monthlyOrder[j] -= delivery;
+                rest += monthlyOrder[j];
+            }
+            
+            need = rest;
+            made = 0;
+        }
+        
+        // latest range price
+        answer += need * cost[cost.length - 1][1];
+        return answer;
+    }
+}
---
+++ LECTURE_TMMRRW/01/SenderTower.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        // 기지국 설치 갯수
+        int answer = 0;
+        int stationIdx = 0;
+        
+        int position = 1;
+        while (position <= n) {
+            if (stationIdx < stations.length && stations[stationIdx] - w <= position) {
+                // 기지국의 전파범위 안에 있음
+                position = stations[stationIdx] + w + 1;
+                stationIdx++;
+            } else {
+                // 기지국 설치
+                answer += 1;
+                position += (w * 2) + 1;
+            }
+        }
+        return answer;
+    }
+}
+/**
+ * 효율성 높이기
+ * loop 개선
+ * 적절한 DS 사용
+ * 불필요한 object 제거
+ */
