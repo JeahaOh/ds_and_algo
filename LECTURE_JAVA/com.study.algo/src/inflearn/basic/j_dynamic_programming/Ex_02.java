c55078e88a55052a3ead8a2e63ddc5b65ee58890
JeahaOh <jeaha1217@gmail.com>
2023-06-20 12:18:26 +0900

    2023 06 20

---
+++ LECTURE_JAVA/com.study.algo/src/inflearn/basic/j_dynamic_programming/Ex_02.java
@@ -0,0 +1,30 @@
+package inflearn.basic.j_dynamic_programming;
+
+import java.util.Scanner;
+
+/**
+ * 돌다리 건너기
+ */
+public class Ex_02 {
+    static int[] dy;
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n + 1; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+        
+        return dy[n + 1];
+    }
+    
+    public static void main(String[] args) {
+        Ex_02 t = new Ex_02();
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        dy = new int[n + 2];
+        
+        System.out.println(t.solution(n));
+    }
+}
