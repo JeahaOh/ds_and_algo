4f2dbc3e085fec8628f8af05dda3ac5f8f606698
JeahaOh <jeaha1217@gmail.com>
2023-07-13 20:59:58 +0900

    2023 07 13

--- LECTURE_JAVA/com.study.algo/src/inflearn/basic/b_array/Ex_07.java
+++ LECTURE_JAVA/com.study.algo/src/inflearn/basic/b_array/Ex_07.java
@@ -3,29 +3,31 @@ package inflearn.basic.b_array;
 import java.util.Scanner;
 
 public class Ex_07 {
-  public static int solution(int n, int[] arr) {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    int[] scores = new int[n];
+    
+    for (int i = 0; i < n; i++) {
+      scores[i] = sc.nextInt();
+    }
+    
+    System.out.println(solution(scores));
+  }
+  
+  public static int solution(int[] scores) {
     int answer = 0;
     int cnt = 0;
-    for (int i = 0; i < n; i++) {
-      if (arr[i] == 0) {
-        cnt = 0;
-      } else {
+    
+    for (int i = 0; i < scores.length; i++) {
+      if (scores[i] == 0) cnt = 0;
+      else {
         cnt++;
         answer += cnt;
       }
     }
+    
     return answer;
-  }
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[] arr = new int[n];
-    
-    for (int i = 0; i < n; i++) {
-      arr[i] = sc.nextInt();
-    }
-    
-    System.out.println(solution(n, arr));
   }
 }
