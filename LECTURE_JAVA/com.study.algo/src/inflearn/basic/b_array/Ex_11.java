6b5ba6428b5926bc0423393cfce989a31b70c813
JeahaOh <jeaha1217@gmail.com>
2023-07-17 21:00:25 +0900

    2023 07 17

--- LECTURE_JAVA/com.study.algo/src/inflearn/basic/b_array/Ex_11.java
+++ LECTURE_JAVA/com.study.algo/src/inflearn/basic/b_array/Ex_11.java
@@ -2,43 +2,46 @@ package inflearn.basic.b_array;
 import java.util.Scanner;
 
 public class Ex_11 {
-  public static int solution(int n, int[][] array) {
-    // 최댓값 비교를 위해 최소값으로 초기화
-    int answer = 0, max = Integer.MIN_VALUE;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     
-    for (int i = 1; i <= n; i++) {
+    int n = sc.nextInt();
+    int[][] board = new int[n + 1][6];
+    
+    // 0 학년으로 계산하는 것 보다 1학년으로 계산하는 것이 편하다.
+    for (int stdntNo = 1; stdntNo <= n; stdntNo++) {
+      for (int grade = 1; grade <= 5; grade++) {
+        board[stdntNo][grade] = sc.nextInt();
+      }
+    }
+    
+    System.out.println(solution(n, board));
+  }
+  
+  public static int solution(int stdntCnt, int[][] board) {
+    int clssPrsidnt = 0;
+    int max = Integer.MIN_VALUE;
+    
+    for (int i = 1; i <= stdntCnt; i++) {
       int cnt = 0;
       
-      for (int j = 1; j <= n; j++) {
-        // i 학생의 k 학년 반을 j 학생의 k 학년의 반과 비교
+      for (int j = 1; j <= stdntCnt; j++) {
         for (int k = 1; k <= 5; k++) {
-          if (array[i][k] == array[j][k]) {
+          // i 학생이 j 학생과 k 학년 때 같은 반이였는가?
+          if (board[i][k] == board[j][k]) {
             cnt++;
-            // 한번이라도 같은 반이였었다면 다음 학년은 카운트 필요가 없다.
+            // 한번이라도 같은 반이었다면 다음 학년은 카운트 할 필요가 없다.
             break;
           }
         }
       }
       
-      // cnt가 제일 많은 학생(i)을 찾는다
       if (cnt > max) {
         max = cnt;
-        answer = i;
+        clssPrsidnt = i;
       }
     }
     
-    return answer;
-  }
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[][] array = new int[n + 1][6];
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= 5; j++) {
-        array[i][j] = sc.nextInt();
-      }
-    }
-    System.out.println(solution(n, array));
+    return clssPrsidnt;
   }
 }
