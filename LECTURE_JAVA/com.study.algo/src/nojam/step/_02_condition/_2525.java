6fa6cbd82efee2cce2d1327116fc8edbf5f4b79d
JeahaOh <jeaha1217@gmail.com>
2024-04-19 21:30:25 +0900

    2024 04 19

---
+++ LECTURE_JAVA/com.study.algo/src/nojam/step/_02_condition/_2525.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * [문제 -> 단계 -> 조건문] 오븐 시계
+ * https://www.acmicpc.net/problem/2525
+ */
+public class _2525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String nowTime = br.readLine();
+        StringTokenizer st = new StringTokenizer(nowTime);
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        //bw.write(String.format("%d %d %d%n", a, b, c));
+
+        // 분으로 계산하여 합을 구한 뒤 시간을 환산한다.
+        int m = b + (a * 60) + c;
+        int h = m / 60;
+        m = m % 60;
+
+        if (h >= 24) {
+            h -= 24;
+        }
+
+        bw.write(String.format("%d %d%n", h, m));
+
+        br.close();
+        bw.close();
+    }
+}
